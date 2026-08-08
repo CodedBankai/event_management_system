476c395c7182d38f20eb189f4b22331636c559a1
Rachit Sinha <rachit.codekarle@gmail.com>
2026-08-08 14:12:23 +0530

    Add auto-superuser creation to build script

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [[ -n "${DJANGO_SUPERUSER_USERNAME}" && -n "${DJANGO_SUPERUSER_PASSWORD}" ]]; then
+    python manage.py createsuperuser --noinput || echo "Superuser already exists or creation failed."
+fi
