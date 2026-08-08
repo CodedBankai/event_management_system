8fc9ee443cd9cc7fe909b105bae963a422ca0f7b
Rachit Sinha <rachit.codekarle@gmail.com>
2026-08-08 13:58:05 +0530

    Fix dj-database-url crash on empty DATABASE_URL

--- event_management_system/settings.py
+++ event_management_system/settings.py
@@ -101,7 +101,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
+        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3'),
         conn_max_age=600,
     )
 }
